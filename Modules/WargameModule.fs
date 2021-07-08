1911c37b81927dc9f10cb862117a86282bd93dfb
Ryan <ryan@youngryan.com>
2021-07-08 11:59:13 -0700

    refactor: put ASCII art directly in source code

--- Modules/WargameModule.fs
+++ Modules/WargameModule.fs
@@ -19,10 +19,16 @@ type public WargameModule(commands: CommandService) =
 
     member private this._Ops() =
         async {
-            let b64 = "IOKWiOKWiOKWiOKWiOKWiOKWiOKVlyDilojilojilojilojilojilojilZcg4paI4paI4paI4paI4paI4paI4paI4pWX4paI4paI4paI4paI4paI4paI4pWXIOKWiOKWiOKVl+KWiOKWiOKWiOKWiOKWiOKWiOKVlyDilojilojilZcK4paI4paI4pWU4pWQ4pWQ4pWQ4paI4paI4pWX4paI4paI4pWU4pWQ4pWQ4paI4paI4pWX4paI4paI4pWU4pWQ4pWQ4pWQ4pWQ4pWd4pWa4pWQ4pWQ4pWQ4pWQ4paI4paI4pWX4paI4paI4pWR4pWa4pWQ4pWQ4pWQ4pWQ4paI4paI4pWX4paI4paI4pWRCuKWiOKWiOKVkSAgIOKWiOKWiOKVkeKWiOKWiOKWiOKWiOKWiOKWiOKVlOKVneKWiOKWiOKWiOKWiOKWiOKWiOKWiOKVlyAg4paE4paI4paI4paI4pWU4pWd4paI4paI4pWRICDiloTilojilojilojilZTilZ3ilojilojilZEK4paI4paI4pWRICAg4paI4paI4pWR4paI4paI4pWU4pWQ4pWQ4pWQ4pWdIOKVmuKVkOKVkOKVkOKVkOKWiOKWiOKVkSAg4paA4paA4pWQ4pWQ4pWdIOKVmuKVkOKVnSAg4paA4paA4pWQ4pWQ4pWdIOKVmuKVkOKVnQrilZrilojilojilojilojilojilojilZTilZ3ilojilojilZEgICAgIOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKVkSAg4paI4paI4pWXICAg4paI4paI4pWXICDilojilojilZcgICDilojilojilZcKIOKVmuKVkOKVkOKVkOKVkOKVkOKVnSDilZrilZDilZ0gICAgIOKVmuKVkOKVkOKVkOKVkOKVkOKVkOKVnSAg4pWa4pWQ4pWdICAg4pWa4pWQ4pWdICDilZrilZDilZ0gICDilZrilZDilZ0=";
-            let text = Encoding.UTF8.GetString (Convert.FromBase64String b64)
             let ctx = this.Context()
-            do! ctx.Channel.SendMessageAsync($"```{text}```")
+            let text = "```
+ ██████╗ ██████╗ ███████╗██████╗ ██╗██████╗ ██╗
+██╔═══██╗██╔══██╗██╔════╝╚════██╗██║╚════██╗██║
+██║   ██║██████╔╝███████╗  ▄███╔╝██║  ▄███╔╝██║
+██║   ██║██╔═══╝ ╚════██║  ▀▀══╝ ╚═╝  ▀▀══╝ ╚═╝
+╚██████╔╝██║     ███████║  ██╗   ██╗  ██╗   ██╗
+ ╚═════╝ ╚═╝     ╚══════╝  ╚═╝   ╚═╝  ╚═╝   ╚═╝
+```"
+            do! ctx.Channel.SendMessageAsync(text)
                 |> Async.AwaitTask
                 |> FSharp.ensureSuccess
         }
