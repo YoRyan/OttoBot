1bd62636f3b94af70b19bef60e523702c9053ea5
Ryan Young <ryan@youngryan.com>
2025-12-10 05:13:23 +0000

    style: remove unnecessary parantheses; prefer pipes

--- Helpers.fs
+++ Helpers.fs
@@ -4,11 +4,11 @@ module OttoBot.Helpers
 /// If the list lengths do not match, the resulting length is equal to that of the longer list.
 let mergeHigher a b =
     let rec _mergeHigher (accum: list<'a>) (a: list<'a>) (b: list<'a>) : list<'a> =
-        match (a, b) with
-        | ([], []) -> List.rev accum
-        | (ah :: at, []) -> _mergeHigher (ah :: accum) at []
-        | ([], bh :: bt) -> _mergeHigher (bh :: accum) [] bt
-        | (ah :: at, bh :: bt) -> _mergeHigher ((max ah bh) :: accum) at bt
+        match a, b with
+        | [], [] -> List.rev accum
+        | ah :: at, [] -> _mergeHigher (ah :: accum) at []
+        | [], bh :: bt -> _mergeHigher (bh :: accum) [] bt
+        | ah :: at, bh :: bt -> _mergeHigher (max ah bh :: accum) at bt
 
     _mergeHigher [] a b
 
@@ -16,11 +16,11 @@ let mergeHigher a b =
 /// in a given value for the missing positions in the shorter list.
 let zipLonger defa defb a b =
     let rec _zipLonger (accum: list<'a * 'b>) (defa: 'a) (defb: 'b) (a: list<'a>) (b: list<'b>) : list<'a * 'b> =
-        match (a, b) with
-        | ([], []) -> List.rev accum
-        | (ah :: at, []) -> _zipLonger ((ah, defb) :: accum) defa defb at []
-        | ([], bh :: bt) -> _zipLonger ((defa, bh) :: accum) defa defb [] bt
-        | (ah :: at, bh :: bt) -> _zipLonger ((ah, bh) :: accum) defa defb at bt
+        match a, b with
+        | [], [] -> List.rev accum
+        | ah :: at, [] -> _zipLonger ((ah, defb) :: accum) defa defb at []
+        | [], bh :: bt -> _zipLonger ((defa, bh) :: accum) defa defb [] bt
+        | ah :: at, bh :: bt -> _zipLonger ((ah, bh) :: accum) defa defb at bt
 
     _zipLonger [] defa defb a b
 
@@ -34,10 +34,10 @@ let makeTable (hsep: string) (vsep: string) (rows: seq<TableRow>) : string =
         Seq.map
             (function
             | Separator -> []
-            | Data(fields) -> Seq.toList (Seq.map String.length fields))
+            | Data fields -> fields |> Seq.map String.length |> Seq.toList)
             rows
 
-    let widths = List.reduce mergeHigher (Seq.toList widthsByRow)
+    let widths = widthsByRow |> Seq.toList |> List.reduce mergeHigher
 
     let render =
         function
@@ -45,8 +45,8 @@ let makeTable (hsep: string) (vsep: string) (rows: seq<TableRow>) : string =
             let total = List.sum widths + vsep.Length * (widths.Length - 1)
 
             String.replicate (total / hsep.Length) hsep
-        | Data(fields) ->
-            Seq.map (fun (s: string, w: int) -> s.PadRight(w)) (zipLonger "" 0 (Seq.toList fields) widths)
+        | Data fields ->
+            Seq.map (fun (s: string, w: int) -> s.PadRight w) (zipLonger "" 0 (Seq.toList fields) widths)
             |> String.concat vsep
 
     let ascii = Seq.map render rows |> String.concat "\n"
